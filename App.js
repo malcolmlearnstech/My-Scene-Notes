e79c30c5c142bb7f2041c413150ffa07f9564070
Malcolm Mack <malcolmtechlearning@gmail.com>
2023-06-21 22:54:01 -0400

    feat: add navigation via App

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TestPage from './Screens/TestPage';
+import Climates from './Screens/Climates';
+import Winter from './Screens/Winter';
+import Spring from './Screens/Spring';
+import Summer from './Screens/Summer';
+import Fall from './Screens/Fall';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+
+export class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="TestPage">
+          <Stack.Screen name="TestPage" component={TestPage} />
+          <Stack.Screen name="Climates" component={Climates} />
+          <Stack.Screen name="Winter" component={Winter} />
+          <Stack.Screen name="Spring" component={Spring} />
+          <Stack.Screen name="Summer" component={Summer} />
+          <Stack.Screen name="Fall" component={Fall} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
